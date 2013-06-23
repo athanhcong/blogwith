8b38704df9d41bd89f25d6ccdf1c06978c1fab1b
Cong Vo <athanhcong@gmail.com>
2013-06-23 15:55:52 +0800

    fix number in timezone

--- everest.js
+++ everest.js
@@ -360,7 +360,7 @@ app.get('/evernote/create-notebook', function(req, res){
   var timezone = query.timezone;
   console.log("timezone" + timezone);
 
-  req.session.user.timezone = timezone;
+  req.session.user.timezoneOffset = timezone;
 
   redisClient.set('users:' + req.session.user.userId + ':evernote:user', JSON.stringify(req.session.user));
 
@@ -606,12 +606,14 @@ var checkUpdateForPost = function(req, note, callback) {
   });
 }
 
+
 var createPostWithMetadata = function(userInfo, noteGuid, callback) {
   var noteStore = EvernoteClient(userInfo.oauthAccessToken).getNoteStore();
   
   //getNote = function(authenticationToken, guid, withContent, withResourcesData, withResourcesRecognition, withResourcesAlternateData, callback) {
   noteStore.getNote(userInfo.oauthAccessToken, noteGuid, true, false, false, false, function(note) {
     console.log('Get note for creating: - Note: ' + note.title);
+    note.timezoneOffset = userInfo.timezoneOffset;
     note.timezone = userInfo.timezone;
     createGithubPost(userInfo.id, note, function(error, data) {
       callback(error, data);
@@ -652,6 +654,7 @@ var updatePostWithMetadata = function(userInfo, noteGuid, callback) {
         });          
       } else {
         console.log('Can not find github sha. Create instead');
+        note.timezoneOffset = userInfo.timezoneOffset;
         note.timezone = userInfo.timezone;
         createGithubPost(userInfo.id, note, function(err, data) {
           callback(err, data);
