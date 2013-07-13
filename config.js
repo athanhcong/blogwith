45d1228bd18db167a09ec0cd6485501c29aa7947
Cong Vo <athanhcong@gmail.com>
2013-07-13 20:49:05 +0800

    add configuration

--- config.js
+++ config.js
@@ -7,7 +7,10 @@ config = {};
       config.serverUrl = process.env.URL;
       config['githubClientId'] = '1144e0f6ba3889d04621';
       config['githubClientSecret'] = '910f3c346e97c8bdfccbb9001d7b010f1ce6a0e3';
-      config.mongoConnectionString = 'mongodb://heroku:1b619f94ee94e192ccc45bafb8553631@dharma.mongohq.com:10097/app16258179';
+      //PRO: 'mongodb://heroku:1b619f94ee94e192ccc45bafb8553631@dharma.mongohq.com:10097/app16258179';
+      
+      //DEV: mongodb://heroku:6b26dcd6b4edbe356ceeaec644521974@dharma.mongohq.com:10068/app16893265
+      config.mongoConnectionString = process.env.MONGO_URL;
   } else {
       config['serverPort'] = '8082';
       config['serverUrl'] = 'http://localhost:' + config.serverPort;
